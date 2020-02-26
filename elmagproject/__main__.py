1299109561198ba394cf78427ec4a7d0f474c510
Vegard Hennestad <vegahen@stud.ntnu.no>
2020-02-26 10:11:48 +0100

    4th

--- elmagproject/__main__.py
+++ elmagproject/__main__.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy
 
-from Particle import Particle
-from consts import m_e, q_e
+from common.Particle import Particle
+from common.consts import m_e, q_e
 
 myPart = Particle(m_e, q_e, 1, 2, 3, 10*m_e, 20*m_e, 30*m_e)
 myPart.print()
